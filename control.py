05dc8632d49d04633881db12e08a2f4c4e506685
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-09-07 17:13:29 +0300

    PyAutoGUI - Control mouse and keyboard

--- control.py
+++ control.py
@@ -3,3 +3,18 @@ import pyautogui
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
+# print(pyautogui.size())
+
+# Specific location
+# for i in range(2): 
+#     pyautogui.moveTo(100, 100, duration=0.25)
+#     pyautogui.moveTo(200, 100, duration=0.25)
+#     pyautogui.moveTo(200, 200, duration=0.25)
+#     pyautogui.moveTo(100, 200, duration=0.25)
+
+# Any location
+# for i in range(2):
+#     pyautogui.moveRel(100, 0, duration=0.25)
+#     pyautogui.moveRel(0, 100, duration=0.25)
+#     pyautogui.moveRel(-100, 0, duration=0.25) 
+#     pyautogui.moveRel(0, -100, duration=0.25)
